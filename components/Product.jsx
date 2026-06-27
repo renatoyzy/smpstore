7db1c5c98cc984723a40c2cba25e7a89bcc90696
Renato <renato.augusto.almeida.santos@gmail.com>
2026-06-27 20:58:03 -0300

    Remove Temp Sec Coupon

--- components/Product.jsx
+++ components/Product.jsx
@@ -54,8 +54,7 @@ export const COUPONS = {
     'EIMANESMP': 0.00,
     'SMPRAIZ': 0.00,
     'LEGIAOTEMPLARIA': 0.00,
-    'CUBOPOLIS': 0.00,
-    'LHAMOTVSEGREDO': 0.9999
+    'CUBOPOLIS': 0.00
 }
 
 /**
